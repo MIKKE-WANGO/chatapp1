af2110c7c24007bd1bfa3f4d2b6b90a4e88cde71
MIKKE-WANGO <71769643+MIKKE-WANGO@users.noreply.github.com>
2022-06-15 23:22:39 +0300

    asgi

--- chatapp1/asgi.py
+++ chatapp1/asgi.py
@@ -10,13 +10,13 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp1.settings')
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 from  .middleware import TokenAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp1.settings')
 
 application = get_asgi_application()
 
